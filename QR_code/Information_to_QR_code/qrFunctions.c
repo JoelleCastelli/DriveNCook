7c971b198b6b7e0d11d0d687875649007925386e
Nouuu <nlarrieulacoste@myges.fr>
2020-03-17 16:42:41 +0100

    png library

---
+++ QR_code/Information_to_QR_code/qrFunctions.c
@@ -0,0 +1,5 @@
+//
+// Created by nou on 17/03/2020.
+//
+
+#include "qrFunctions.h"
---
+++ QR_code/Information_to_QR_code/qrFunctions.h
@@ -0,0 +1,8 @@
+//
+// Created by nou on 17/03/2020.
+//
+
+#ifndef CSV_TO_PNG_QRFUNCTIONS_H
+#define CSV_TO_PNG_QRFUNCTIONS_H
+
+#endif //CSV_TO_PNG_QRFUNCTIONS_H
