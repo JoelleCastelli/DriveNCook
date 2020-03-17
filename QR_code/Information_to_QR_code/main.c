f7001a7c3bdee69ab1901a492bb8a59035345b3e
Nouuu <nlarrieulacoste@myges.fr>
2020-03-17 16:43:19 +0100

    update project name + main test

--- QR_code/Information_to_QR_code/main.c
+++ QR_code/Information_to_QR_code/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "qrFunctions.h"
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    createQR("test",
+             "Ceci est un gros test.\nPour l'instant, la taille maximale d'un message est de :\nMAX = 213 caractères");
+
+    return EXIT_SUCCESS;
 }
