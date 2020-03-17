9a2b3b283531bb9f15e9bec087a810166ab7e8b6
Nouuu <nlarrieulacoste@myges.fr>
2020-03-17 16:43:05 +0100

    my functions library

--- QR_code/Information_to_QR_code/qrFunctions.h
+++ QR_code/Information_to_QR_code/qrFunctions.h
@@ -5,4 +5,25 @@
 #ifndef CSV_TO_PNG_QRFUNCTIONS_H
 #define CSV_TO_PNG_QRFUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "qrcode.h"
+#include "lodepng.h"
+
+typedef struct {
+    u_int8_t r;
+    u_int8_t g;
+    u_int8_t b;
+    u_int8_t transparency;
+} RGBA_pixel;
+
+void createQR(char *filename, char *message);
+
+u_int8_t getVersion(char *message);
+
+u_int8_t QRToPNG(QRCode qrcode, char *filename);
+
+
 #endif //CSV_TO_PNG_QRFUNCTIONS_H
