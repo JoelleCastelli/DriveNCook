4cdf4dc6f4185edf00780b7ac2382088ac677ab9
Antoine <fevre.pasquet@gmail.com>
2020-04-20 18:06:23 +0200

    add model location

---
+++ drivencook/app/Models/Location.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    protected $table = 'truck';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'address', 'city_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    /*protected $hidden = [
+
+    ];*/
+}
