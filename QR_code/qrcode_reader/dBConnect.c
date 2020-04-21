e5c997b7081075cd6052185505222b1e91e708f7
Antoine_Ubuntu <fevre.pasquet@gmail.com>
2020-04-21 19:54:33 +0200

    add log to dBConnect files

--- QR_code/qrcode_reader/dBConnect.c
+++ QR_code/qrcode_reader/dBConnect.c
@@ -6,25 +6,30 @@ Author: Antoine FEVRE
 
 #include "dBConnect.h"
 
-int callMySQL(MYSQL ** conn) {
+int callMySQL(MYSQL ** conn, FILE *logFd) {
+    char tmpLog[100];
     char * server = "134.122.107.73";
-    char * user = "user";
+    char * user = "mysql";
     char * password = "ESGIgroupe6";
     char * database = "drivencook";
 
     *conn = mysql_init(NULL);
 
     if(!mysql_real_connect(*conn, server, user, password, database, 0, NULL, 0)) {
-        fprintf(stderr, "%s\n", mysql_error(*conn));
+        sprintf(tmpLog, "%s\n", mysql_error(*conn));
+        toLog(logFd, ERROR, tmpLog);
+        toLog(logFd, INFO, "Program stop...");
         exit(EXIT_FAILURE);
     }
 
     return EXIT_SUCCESS;
 }
 
-void query(MYSQL ** conn, char * request, MYSQL_RES ** res) {
+void query(MYSQL ** conn, char * request, MYSQL_RES ** res, FILE *logFd) {
+    char tmpLog[100];
     if(mysql_query(*conn, request)) {
-        fprintf(stderr, "MYSQL QUERY ERROR : %s\nREQUEST : %s\n", mysql_error(*conn), request);
+        sprintf(tmpLog, "MYSQL QUERY ERROR : %s\nREQUEST : %s\n", mysql_error(*conn), request);
+        toLog(logFd, ERROR, tmpLog);
         exit(EXIT_FAILURE);
     }
 
--- QR_code/qrcode_reader/dBConnect.h
+++ QR_code/qrcode_reader/dBConnect.h
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <mysql/mysql.h>
+#include "useful.h"
 
 #ifndef DBCONNECT_H
 #define DBCONNECT_H
 
-int callMySQL(MYSQL ** conn);
-void query(MYSQL ** conn, char * request, MYSQL_RES ** res);
+int callMySQL(MYSQL ** conn, FILE *logFd);
+
+void query(MYSQL ** conn, char * request, MYSQL_RES ** res, FILE *logFd);
 
 #endif //DBCONNECT_H
