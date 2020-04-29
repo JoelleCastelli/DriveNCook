15a30c15be77ca44442f5cdc78167f43326904ae
Antoine <fevre.pasquet@gmail.com>
2020-04-29 18:52:35 +0200

    add warehouse translation files

---
+++ drivencook/resources/lang/en/warehouse_creation.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'name' => 'Name',
+    'city' => 'City',
+    'address' => 'Address',
+    'submit' => 'Add',
+
+    'name_error' => 'Name invalid format',
+    'city_error' => 'City invalid format',
+    'address_error' => 'Address invalid format',
+
+    'new_warehouse_success' => 'New warehouse added',
+    'new_warehouse_error' => 'Error adding new warehouse',
+
+    // Tooltips
+    'set_name' => 'Set name',
+    'set_address' => 'Set address',
+
+    // Dropdown menu
+    'select_menu_off' => 'Choose',
+
+];
---
+++ drivencook/resources/lang/fr/warehouse_creation.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'name' => 'Nom',
+    'city' => 'Ville',
+    'address' => 'Adresse',
+    'submit' => 'Ajouter',
+
+    'name_error' => 'Nom : format invalide',
+    'city_error' => 'Ville : format invalide',
+    'address_error' => 'Adresse : format invalide',
+
+    'new_warehouse_success' => 'Nouvel entrepôt ajouté',
+    'new_warehouse_error' => 'Erreur lors de l\'ajout de l\'entrepôt',
+
+    // Tooltips
+    'set_name' => 'Entrez le nom',
+    'set_address' => 'Entrez l\'adresse',
+
+    // Dropdown menu
+    'select_menu_off' => 'Choisissez',
+
+];
