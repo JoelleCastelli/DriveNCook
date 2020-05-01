65bfe9b394d9acadccc8497f381568f455090e22
Antoine <fevre.pasquet@gmail.com>
2020-05-02 00:13:14 +0200

    global file

---
+++ drivencook/config/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    $GLOBALS['DISH_TYPE'] = [
+        'hot_dish' => 'warehouse_dishes.hot_dish',
+        'cold_dish' => 'warehouse_dishes.cold_dish',
+        'salty_snack' => 'warehouse_dishes.sweet_snack',
+        'sweet_snack' => 'warehouse_dishes.salty_snack',
+        'drink' => 'warehouse_dishes.drink',
+    ]
+];
