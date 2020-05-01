e7f9acbd2efc8da2516636be53dc7716458025ef
Joelle Castelli <jcastelli1@myges.fr>
2020-05-01 18:23:27 +0200

    add module for JS localization

--- drivencook/resources/views/corporate/layout_corporate.blade.php
+++ drivencook/resources/views/corporate/layout_corporate.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <title>Drive 'N' Cook Corporate</title>
+    <script src="{{asset('js/trad.js')}}"></script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
