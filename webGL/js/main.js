17e44e9b7b965c1dd291e93f9cb30dadc6e393bb
Nouu <noelarrieulacoste@yahoo.fr>
2020-05-02 17:22:52 +0200

    refactor code, functions file

--- webGL/js/main.js
+++ webGL/js/main.js
@@ -1,14 +1,12 @@
 import * as THREE from './libs/three.module.js';
 import {THREEx} from './libs/THREEx.KeyboardState.js';
 import Stats from './libs/stats.module.js';
+import * as functions from './functions.js';
 
 Math.radians = (degrees) => degrees * Math.PI / 180;
 
 let keyboard = new THREEx.KeyboardState(); // import de la librairie qui écoute le clavier
-let camera, geometry, renderer, scene, stats, terrain;
-let vectorX = new THREE.Vector3(1, 0, 0);
-let vectorY = new THREE.Vector3(0, 1, 0);
-let vectorZ = new THREE.Vector3(0, 0, 1);
+let camera, geometry, light1, renderer, scene, stats, terrain;
 
 let terrainDim = {
     width: 100,
@@ -37,27 +35,21 @@ function init() {
     /**
      * init camera
      */
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, terrainDim.width + terrainDim.height)
-    camera.position.set(terrainDim.width / 2, terrainDim.height / 2, 10);
+    camera = functions.createCamera(60, 1, terrainDim.width + terrainDim.height, terrainDim.width / 2, terrainDim.height / 2, 10)
 
     /**
      * init terrain
      */
-    geometry = new THREE.PlaneBufferGeometry(terrainDim.width, terrainDim.height, 32, 32);
-    terrain = new THREE.Mesh(geometry, greenMat);
-    terrain.receiveShadow = true;
-    terrain.position.set(terrainDim.width / 2, terrainDim.height / 2, -80)
-    scene.add(terrain);
+    terrain = functions.createTerrain(terrainDim.width, terrainDim.height, greenMat, terrainDim.width / 2, terrainDim.height / 2, -80);
 
+    scene.add(terrain);
 
     /**
      * light
      */
-    geometry = new THREE.SphereBufferGeometry(2, 32, 32);
-    var light1 = new THREE.PointLight(0xFFFFFF, 0.5);
-    light1.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xFFFFFF})));
-    light1.position.set(0, 20, 0);
-    light1.castShadow = true;
+
+    light1 = functions.createPointLight(2, 0xFEFEFE, 0.5, 0, 20, 0);
+
     scene.add(light1);
 
     /**
@@ -79,49 +71,11 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
-    camControl()
+    functions.camControl(keyboard, camera, cameraT);
     render();
 }
 
 function render() {
     stats.update();
     renderer.render(scene, camera);
-}
-
-function camControl() {
-
-    if (keyboard.pressed("up")) {
-        if (camera.rotation.x < Math.radians(40)) {
-            camera.rotateOnAxis(vectorX, cameraT.rotationSpeed);
-        }
-    }
-    if (keyboard.pressed("down")) {
-        if (camera.rotation.x > Math.radians(-40)) {
-            camera.rotateOnAxis(vectorX, -cameraT.rotationSpeed);
-        }
-    }
-    if (keyboard.pressed("left")) {
-        camera.rotateOnAxis(vectorY, cameraT.rotationSpeed);
-    }
-    if (keyboard.pressed("right")) {
-        camera.rotateOnAxis(vectorY, -cameraT.rotationSpeed);
-    }
-    if (keyboard.pressed("z")) {
-        camera.translateZ(-cameraT.moveSpeed);
-    }
-    if (keyboard.pressed("s")) {
-        camera.translateZ(cameraT.moveSpeed);
-    }
-    if (keyboard.pressed("q")) {
-        camera.translateX(-cameraT.moveSpeed);
-    }
-    if (keyboard.pressed("d")) {
-        camera.translateX(cameraT.moveSpeed);
-    }
-    if (keyboard.pressed("a")) {
-        camera.rotateOnAxis(vectorZ, cameraT.rotationSpeed * 0.5);
-    }
-    if (keyboard.pressed("e")) {
-        camera.rotateOnAxis(vectorZ, -cameraT.rotationSpeed * 0.5);
-    }
 }
