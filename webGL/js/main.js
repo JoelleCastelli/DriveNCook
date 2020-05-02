4f9e3194f59e7f27b30473e1705e710108cb839f
Nouu <noelarrieulacoste@yahoo.fr>
2020-05-02 16:06:41 +0200

    init project and libs

---
+++ webGL/js/main.js
@@ -0,0 +1,21 @@
+import * as THREE from './libs/three.module.js';
+import Stats from './libs/stats.module.js';
+
+var stats
+
+init();
+animate();
+
+function init() {
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+}
+
+function animate() {
+
+}
+
+function render() {
+
+}
