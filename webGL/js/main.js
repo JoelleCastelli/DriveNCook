fc81ee26c26ccab43ca3f7a77d3b56e0b53eabfa
Nouu <noelarrieulacoste@yahoo.fr>
2020-05-02 17:01:06 +0200

    cam control (z,q,s,d + up,down,left,right + a,e)

--- webGL/js/main.js
+++ webGL/js/main.js
@@ -1,19 +1,23 @@
 import * as THREE from './libs/three.module.js';
+import {THREEx} from './libs/THREEx.KeyboardState.js';
 import Stats from './libs/stats.module.js';
 
 Math.radians = (degrees) => degrees * Math.PI / 180;
 
-var camera, geometry, renderer, scene, stats, terrain;
+let keyboard = new THREEx.KeyboardState(); // import de la librairie qui écoute le clavier
+let camera, geometry, renderer, scene, stats, terrain;
+let vectorX = new THREE.Vector3(1, 0, 0);
+let vectorY = new THREE.Vector3(0, 1, 0);
+let vectorZ = new THREE.Vector3(0, 0, 1);
 
-var terrainDim = {
+let terrainDim = {
     width: 100,
     height: 100,
 };
 
-var cameraPos = {
-    x: terrainDim.width / 2,
-    y: terrainDim.height / 2,
-    z: 10
+let cameraT = {
+    moveSpeed: 1,
+    rotationSpeed: 0.01
 };
 
 
@@ -33,8 +37,8 @@ function init() {
     /**
      * init camera
      */
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, terrainDim.width + terrainDim.height)
-    camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, terrainDim.width + terrainDim.height)
+    camera.position.set(terrainDim.width / 2, terrainDim.height / 2, 10);
 
     /**
      * init terrain
@@ -42,9 +46,8 @@ function init() {
     geometry = new THREE.PlaneBufferGeometry(terrainDim.width, terrainDim.height, 32, 32);
     terrain = new THREE.Mesh(geometry, greenMat);
     terrain.receiveShadow = true;
-    terrain.position.set(terrainDim.width/2, terrainDim.height/2, -80)
+    terrain.position.set(terrainDim.width / 2, terrainDim.height / 2, -80)
     scene.add(terrain);
-
 
 
     /**
@@ -76,10 +79,49 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
+    camControl()
     render();
 }
 
 function render() {
     stats.update();
     renderer.render(scene, camera);
+}
+
+function camControl() {
+
+    if (keyboard.pressed("up")) {
+        if (camera.rotation.x < Math.radians(40)) {
+            camera.rotateOnAxis(vectorX, cameraT.rotationSpeed);
+        }
+    }
+    if (keyboard.pressed("down")) {
+        if (camera.rotation.x > Math.radians(-40)) {
+            camera.rotateOnAxis(vectorX, -cameraT.rotationSpeed);
+        }
+    }
+    if (keyboard.pressed("left")) {
+        camera.rotateOnAxis(vectorY, cameraT.rotationSpeed);
+    }
+    if (keyboard.pressed("right")) {
+        camera.rotateOnAxis(vectorY, -cameraT.rotationSpeed);
+    }
+    if (keyboard.pressed("z")) {
+        camera.translateZ(-cameraT.moveSpeed);
+    }
+    if (keyboard.pressed("s")) {
+        camera.translateZ(cameraT.moveSpeed);
+    }
+    if (keyboard.pressed("q")) {
+        camera.translateX(-cameraT.moveSpeed);
+    }
+    if (keyboard.pressed("d")) {
+        camera.translateX(cameraT.moveSpeed);
+    }
+    if (keyboard.pressed("a")) {
+        camera.rotateOnAxis(vectorZ, cameraT.rotationSpeed * 0.5);
+    }
+    if (keyboard.pressed("e")) {
+        camera.rotateOnAxis(vectorZ, -cameraT.rotationSpeed * 0.5);
+    }
 }
