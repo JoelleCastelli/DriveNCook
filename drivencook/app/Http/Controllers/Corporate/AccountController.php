d6a4d40097de3f2a4bd79474759018f9622a68b7
Antoine <fevre.pasquet@gmail.com>
2020-05-07 03:07:54 +0200

    update dashboard with warehouses

--- drivencook/app/Http/Controllers/Corporate/AccountController.php
+++ drivencook/app/Http/Controllers/Corporate/AccountController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Corporate;
 use App\Models\Pseudo;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\Warehouse;
 use Illuminate\Support\Facades\App;
 
 class AccountController extends Controller
@@ -19,10 +20,12 @@ class AccountController extends Controller
     public function dashboard()
     {
         $franchisees = User::where('role', 'Franchisé')->count();
+        $warehouses = Warehouse::count();
         $revenues = App::call('App\Http\Controllers\Corporate\FranchiseeController@get_franchisees_current_month_sale_revenues');
 
         return view('corporate.dashboard')
             ->with('nbfranchisees', $franchisees)
+            ->with('nbWarehouses', $warehouses)
             ->with('revenues', $revenues);
     }
 }
