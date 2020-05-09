823caa63fe72107874ddc15f9a8742a0f3c86581
Nouu <noelarrieulacoste@yahoo.fr>
2020-05-09 18:38:16 +0200

    stock dashboard view stocks

--- drivencook/resources/views/franchise/stock/stock_dashboard.blade.php
+++ drivencook/resources/views/franchise/stock/stock_dashboard.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
 
-        <div class="col-12 mb-5">
+        <div class="col-12 col-lg-6 mb-5">
             <div class="card">
                 <div class="card-header">
                     <h2>Historique des commandes</h2>
@@ -58,6 +58,43 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-12 col-lg-6 mb-5">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Stocks</h2>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="stocks" class="table table-hover table-striped table-bordered table-dark"
+                               style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>Plat</th>
+                                <th>Quantité</th>
+                                <th>Prix de vente</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($stock as $stock_dish)
+                                <tr id="row_{{$stock_dish['dish_id']}}">
+                                    <td>{{$stock_dish['dish']['name']}}</td>
+                                    <td>{{$stock_dish['quantity']}}</td>
+                                    <td class="d-flex justify-content-between">
+                                        {{$stock_dish['unit_price']}} €
+                                        <a href="{{route('franchise.stock_update',["dish_id"=>$stock_dish['dish_id']])}}">
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -65,6 +102,7 @@
     <script type="text/javascript">
         $(document).ready(function () {
             $('#purchase_orders').DataTable();
+            $('#stocks').DataTable();
         });
     </script>
 @endsection
