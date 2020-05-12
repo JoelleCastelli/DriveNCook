61003b4f517a98bbb10b7a8581de396182b33087
Nouu <noelarrieulacoste@yahoo.fr>
2020-05-12 19:14:03 +0200

    franchise stock order v1 working

--- drivencook/app/Http/Controllers/Franchise/StockController.php
+++ drivencook/app/Http/Controllers/Franchise/StockController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Franchise;
 
 use App\Http\Controllers\Controller;
 use App\Http\Middleware\AuthFranchise;
+use App\Models\Dish;
 use App\Models\FranchiseeStock;
 use App\Models\PurchasedDish;
 use App\Models\PurchaseOrder;
+use App\Models\Warehouse;
+use App\Models\WarehousStock;
 use App\Traits\EnumValue;
 use App\Traits\UserTools;
+use Carbon\Carbon;
 
 class StockController extends Controller
 {
@@ -40,12 +44,99 @@ class StockController extends Controller
 
     public function stock_order()
     {
-        return 'stock_order';
+        $warehouse_list = Warehouse::with('city')->get()->toArray();
+//        var_dump($warehouse_list);die;
+        return view('franchise.stock.stock_order_form')
+            ->with("warehouse_list", $warehouse_list);
+    }
+
+    public function stock_order_warehouse($warehouse_id)
+    {
+        $warehouse = Warehouse::whereKey($warehouse_id)
+            ->with('city')
+            ->with('stock')
+            ->first();
+        if ($warehouse == null) {
+            abort(404);
+        }
+        $warehouse = $warehouse->toArray();
+//        var_dump($warehouse);die;
+        return view('franchise.stock.stock_order_form')
+            ->with('warehouse', $warehouse);
+        //json
     }
 
     public function stock_order_submit()
     {
-        return 'stock_order_submit';
+        $order = [];
+        $order['warehouse_id'] = request('warehouse_id');
+        $order['total'] = 0;
+        $order['dishes'] = [];
+
+        foreach (request()->except(['_token', 'warehouse_id']) as $dish_id => $quantity) {
+            if ($quantity > 0) {
+                $dish_id = explode('_', $dish_id)[1];
+                $warehouse_stock = $this->get_warehouse_product_stock($dish_id, $order['warehouse_id']);
+                if ($warehouse_stock == null || $warehouse_stock['quantity'] < $quantity) {
+                    flash("Erreur, un des produits commandés n'est pas disponible dans cet entrepôt ou le stock est insuffisant")->error();
+                    return back();
+                }
+
+                $order['dishes'][] = array(
+                    "id" => $dish_id,
+                    "quantity" => $quantity,
+                    "price" => $warehouse_stock['warehouse_price'],
+                    "dish" => Dish::find($dish_id)->toArray()
+                );
+                $order['total'] += $quantity * $warehouse_stock['warehouse_price'];
+            }
+        }
+        if (count($order['dishes']) == 0) {
+            flash("Aucun plats choisis")->error();
+            return back();
+        }
+//        var_dump($order);die;
+        request()->session()->put('order', $order);
+
+        return view('franchise.stock.stock_order_payment')
+            ->with('order', $order);
+    }
+
+    public function stock_order_validate()
+    {
+        $order = request()->session()->pull('order', null);
+        if ($order == null) {
+            flash("Erreur, la commande a expiré, veuillez réessayer")->warning();
+            return redirect(route('franchise.stock_order'));
+        }
+
+        $order_id = PurchaseOrder::insertGetId([
+            'user_id' => $this->get_connected_user()['id'],
+            'warehouse_id' => $order['warehouse_id'],
+            'date' => Carbon::now()->toDateString()
+        ]);
+        foreach ($order['dishes'] as $order_dish) {
+            PurchasedDish::insert([
+                'purchase_order_id' => $order_id,
+                'dish_id' => $order_dish['id'],
+                'quantity' => $order_dish['quantity'],
+                'unit_price' => $order_dish['price']
+            ]);
+        }
+        flash('Commande créé !')->success();
+        return redirect(route('franchise.stock_dashboard'));
+    }
+
+    public function get_warehouse_product_stock($dish_id, $warehouse_id)
+    {
+        $warehouse_stock = WarehousStock::where([
+            ['dish_id', $dish_id],
+            ['warehouse_id', $warehouse_id]
+        ])->first();
+        if ($warehouse_stock != null) {
+            $warehouse_stock = $warehouse_stock->toArray();
+        }
+        return $warehouse_stock;
     }
 
     public function stock_order_cancel($order_id)
@@ -78,17 +169,11 @@ class StockController extends Controller
             ->with('order', $order);
     }
 
-    public function stock_order_warehouse($warehouse_id)
-    {
-        return 'stock_order_warehouse ' . $warehouse_id;
-        //json
-    }
-
     public function stock_update_submit()
     {
         request()->validate([
-            'dish_id' => ['required','integer'],
-            'unit_price' => ['required','numeric']
+            'dish_id' => ['required', 'integer'],
+            'unit_price' => ['required', 'numeric']
         ]);
         FranchiseeStock::where([
             ['user_id', $this->get_connected_user()['id']],
--- drivencook/routes/franchise/stock.php
+++ drivencook/routes/franchise/stock.php
@@ -5,30 +5,37 @@ Route::get('/franchise/stock', [
     'uses' => 'Franchise\StockController@stock_dashboard'
 ]);
 
-Route::get('/franchise/stock/order', [
-    'as' => 'franchise.stock_order',
-    'uses' => 'Franchise\StockController@stock_order'
-]);
 
 Route::get('/franchise/stock/order/{order_id}', [
     'as' => 'franchise.stock_order_view',
     'uses' => 'Franchise\StockController@stock_order_view'
 ]);
+Route::get('/franchise/stock/new_order', [
+    'as' => 'franchise.stock_order',
+    'uses' => 'Franchise\StockController@stock_order'
+]);
 
-Route::post('/franchise/stock/order', [
+Route::post('/franchise/stock/new_order', [
     'as' => 'franchise.stock_order_submit',
     'uses' => 'Franchise\StockController@stock_order_submit'
 ]);
+
+Route::get('/franchise/stock/new_order/{warehouse_id}', [
+    'as' => 'franchise.stock_order_select_warehouse',
+    'uses' => 'Franchise\StockController@stock_order_warehouse'
+]);
+
+Route::get('/franchise/stock/new_order_validate', [
+    'as' => 'franchise.stock_order_validate',
+    'uses' => 'Franchise\StockController@stock_order_validate'
+]);
+
 
 Route::delete('/franchise/stock/order_cancel/{order_id}', [
     'as' => 'franchise.stock_order_cancel',
     'uses' => 'Franchise\StockController@stock_order_cancel'
 ]);
 
-Route::get('/franchise/stock/order/{warehouse_id}', [
-    'as' => 'franchise.stock_order_select_warehouse',
-    'uses' => 'Franchise\StockController@stock_order_warehouse'
-]);
 
 Route::post('/franchise/stock/update', [
     'as' => 'franchise.stock_update_submit',
