cbcc0d1c354718ad4e3923e9dbe1b4aac5c784bd
Joëlle Castelli <castelli.jo@gmail.com>
2020-05-24 21:03:04 +0200

    franchisee invoice list

--- drivencook/app/Http/Controllers/Franchise/InvoiceController.php
+++ drivencook/app/Http/Controllers/Franchise/InvoiceController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Franchise;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use App\Models\Pseudo;
+use App\Traits\UserTools;
+use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    public function invoices_list() {
-        // récupérer le mail en session
-        return view('franchise.invoices_list');
-    }
+    use UserTools;
 
+    public function invoices_list() {
+        $franchisee_id = $this->get_connected_user()['id'];
+        $invoices = Invoice::with('user')->where('user_id', $franchisee_id)->get()->toArray();
+        return view('franchise.invoices_list')->with('invoices', $invoices);
+    }
 }
--- drivencook/resources/views/franchise/invoices_list.blade.php
+++ drivencook/resources/views/franchise/invoices_list.blade.php
@@ -5,5 +5,59 @@
 
 
 @section('content')
-    coucou
+    <div class="row">
+        <div class="col-12 col-lg-12 mb-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="franchisee_invoices_list" class="table table-hover table-striped table-bordered table-dark"
+                               style="width: 100%">
+                            <thead>
+                                <tr>
+                                    <th>Date d'émission</th>
+                                    <th>Référence</th>
+                                    <th>Montant</th>
+                                    <th>Type de facture</th>
+                                    <th>Statut</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($invoices as $invoice)
+                                    <tr>
+                                        <td>
+                                            {{DateTime::createFromFormat('Y-m-d',$invoice['date_emitted'])->format('d/m/Y')}}
+                                        </td>
+                                        <td>{{ $invoice['reference'] }}</td>
+                                        <td>{{ number_format($invoice['amount'], 2, ',', ' ') }} €</td>
+                                        <td>@if ($invoice['monthly_fee'] == 1)
+                                                Redevance périodique
+                                            @elseif ($invoice['initial_fee'] == 1)
+                                                Redevance initiale forfaitaire
+                                            @else
+                                                Réassort
+                                            @endif</td>
+                                        <td>{{ $invoice['status'] }}</td>
+                                        <td class="text-center">
+                                            <a class="ml-2" href="#">
+                                                <button class="text-light fa fa-file-pdf ml-3"></button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#franchisee_invoices_list').DataTable();
+        });
+    </script>
 @endsection
