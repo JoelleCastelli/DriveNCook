169d97f427b34ff936fd2775ae1d5755c6906ee2
Antoine <fevre.pasquet@gmail.com>
2020-05-25 00:08:36 +0200

    add trad file

--- drivencook/app/Http/Controllers/Client/OrderController.php
+++ drivencook/app/Http/Controllers/Client/OrderController.php
@@ -4,8 +4,12 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\AuthClient;
 
 class OrderController extends Controller
 {
-
+    public function __construct()
+    {
+        $this->middleware(AuthClient::class);
+    }
 }
