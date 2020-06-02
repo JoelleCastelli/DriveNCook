5ffeabd143b5f7efc5116d88aab67d4b0b124eb7
Antoine <fevre.pasquet@gmail.com>
2020-06-02 22:26:20 +0200

    update client global trads

--- drivencook/resources/lang/en/client/global.php
+++ drivencook/resources/lang/en/client/global.php
@@ -4,4 +4,6 @@ return [
     'join_message' => 'You are looking for fresh products with an exceptional taste, served quickly! So join the movement and come share a meal with us.',
 
     'back_dashboard' => 'Home',
+
+    'actions' => 'Actions',
 ];
--- drivencook/resources/lang/fr/client/global.php
+++ drivencook/resources/lang/fr/client/global.php
@@ -4,4 +4,7 @@ return [
     'join_message' => 'Vous cherchez des produits frais avec un goût d\'exception, servis rapidement ! Alors rejoignez le mouvement et venez partager un repas avec nous.',
 
     'back_dashboard' => 'Accueil',
+
+    'actions' => 'Actions',
+
 ];
