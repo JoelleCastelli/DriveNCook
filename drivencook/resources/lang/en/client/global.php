f82de10d7ab8e13c060956eed09ea40f37391dcd
Antoine <fevre.pasquet@gmail.com>
2020-06-07 23:37:09 +0200

    add trads

--- drivencook/resources/lang/en/client/global.php
+++ drivencook/resources/lang/en/client/global.php
@@ -2,6 +2,7 @@
 
 return [
     'join_message' => 'You are looking for fresh products with an exceptional taste, served quickly! So join the movement and come share a meal with us.',
+    'welcome' => 'Welcome',
 
     'back_dashboard' => 'Home',
     'delete_account' => 'Delete your account',
@@ -9,4 +10,7 @@ return [
     'history' => 'History',
 
     'actions' => 'Actions',
+
+    'loyalty_point' => 'Your loyalty points',
+    'show_details' => 'Details'
 ];
--- drivencook/resources/lang/fr/client/global.php
+++ drivencook/resources/lang/fr/client/global.php
@@ -2,6 +2,7 @@
 
 return [
     'join_message' => 'Vous cherchez des produits frais avec un goût d\'exception, servis rapidement ! Alors rejoignez le mouvement et venez partager un repas avec nous.',
+    'welcome' => 'Bienvenue',
 
     'back_dashboard' => 'Accueil',
     'delete_account' => 'Supprimer votre compte',
@@ -10,4 +11,6 @@ return [
 
     'actions' => 'Actions',
 
+    'loyalty_point' => 'Vos points de fidélité',
+    'show_details' => 'Détails'
 ];
