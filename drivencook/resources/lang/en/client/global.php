d5c9806072b55380b08bd352e6589d678c0ae150
Antoine <fevre.pasquet@gmail.com>
2020-06-14 14:49:08 +0200

    add global trads

--- drivencook/resources/lang/en/client/global.php
+++ drivencook/resources/lang/en/client/global.php
@@ -3,6 +3,7 @@
 return [
     'join_message' => 'You are looking for fresh products with an exceptional taste, served quickly! So join the movement and come share a meal with us.',
     'welcome' => 'Welcome',
+    'login_client' => 'Login',
 
     'back_dashboard' => 'Home',
     'delete_account' => 'Delete your account',
