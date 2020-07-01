fecc0e8548848c7a1a8a6244b9ea11d8c9d5536b
Antoine <fevre.pasquet@gmail.com>
2020-07-01 20:37:07 +0200

    add account trans

--- drivencook/resources/lang/fr/client/account.php
+++ drivencook/resources/lang/fr/client/account.php
@@ -12,12 +12,21 @@ return [
     'email' => 'E-mail',
     'telephone' => 'Téléphone',
 
+
     'new_password' => 'Nouveau mot de passe',
     'password_confirm' => 'Password confirmation',
 
-    'set_new_password' => 'Entrez votre nouveau mot de passe',
-    'set_password_confirm' => 'Confirmez votre nouveau mot de passe',
 
     'update_submit' => 'Mettre à jour le compte',
     'password_submit' => 'Mettre à jour le mot de passe',
+
+    // Tooltips
+    'set_name' => 'Entrez un nom',
+    'set_firstname' => 'Entrez un prénom',
+    'set_email' => 'Entrez un e-mail',
+    'set_telephone' => 'Entrez un numéro de téléphone',
+    'opt_in_message' => 'En cochant cette case, j\'accepte recevoir la newsletter de Drive \'N\' Cook',
+
+    'set_new_password' => 'Entrez votre nouveau mot de passe',
+    'set_password_confirm' => 'Confirmez votre nouveau mot de passe',
 ];
