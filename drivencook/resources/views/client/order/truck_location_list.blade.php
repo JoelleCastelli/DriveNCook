bbfa97b5047284046029320f8595f5ac0c2bd345
Nouu <noelarrieulacoste@yahoo.fr>
2020-07-04 21:57:43 +0200

    Client side truck map location done

--- drivencook/resources/views/client/order/truck_location_list.blade.php
+++ drivencook/resources/views/client/order/truck_location_list.blade.php
@@ -16,49 +16,7 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-12">
-                            <div class="table-responsive">
-                                <table id="allTrucks" class="table table-hover table-striped table-bordered table-dark"
-                                       style="width: 100%">
-                                    <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>{{ trans('client/order.pseudo') }}</th>
-                                        <th>{{ trans('client/order.franchisee') }}</th>
-                                        <th>{{ trans('client/order.location_name') }}</th>
-                                        <th>{{ trans('client/order.location_address') }}</th>
-                                        <th>{{ trans('client/order.brand') }}</th>
-                                        <th>{{ trans('client/order.model') }}</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($trucks as $truck)
-                                            @if(!empty($truck['user'])
-                                             && !empty($truck['location']['address'])
-                                             && !empty($truck['location']['name'])
-                                             && !empty($truck['location']['postcode'])
-                                             && !empty($truck['location']['city'])
-                                             && !empty($truck['location']['country']))
-                                            <tr>
-                                                <td>
-                                                    <a href="{{ route('client_order',['id'=>$truck['id']]) }}" style="color: inherit">
-                                                        <i class="fa fa-eye"></i>
-                                                    </a>
-                                                </td>
-                                                <td>{{ $truck['user']['pseudo']['name'] }}</td>
-                                                <td>{{ $truck['user']['firstname'] . '  ' . $truck['user']['lastname'] }}</td>
-                                                <td>{{ $truck['location']['name'] }}</td>
-                                                <td>{{ $truck['location']['address'] . ' - '
-                                                . $truck['location']['postcode'] . ' - '
-                                                . $truck['location']['city'] . ' - '
-                                                . $truck['location']['country'] }}
-                                                </td>
-                                                <td>{{ $truck['brand'] }}</td>
-                                                <td>{{ $truck['model'] }}</td>
-                                            </tr>
-                                            @endif
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                            <div id="map_view" style="width: 100%; height:600px;">
                             </div>
                         </div>
                     </div>
@@ -69,10 +27,45 @@
 @endsection
 
 @section('script')
+    <script src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_MAPS_API_KEY')}}">google.maps.event.addDomListener(window, 'load', initMap);</script>
     <script type="text/javascript">
-        $(document).ready(function () {
-            let table = $('#allTrucks').DataTable({searchPanes: true});
-            table.searchPanes.container().prependTo(table.table().container());
+        var locations = [
+                @foreach($trucks as $truck)
+                @if(!empty($truck['user']))
+            ['{{$truck['location']['name']}}', '{{$truck['location']['latitude']}}', '{{$truck['location']['longitude']}}', '{{route('client_order',['truck_id'=>$truck['id']])}}', '{{$truck['user']['pseudo']['name']}}'],
+            @endif
+            @endforeach
+        ];
+
+        var map = new google.maps.Map(document.getElementById('map_view'), {
+            zoom: 10,
+            center: new google.maps.LatLng(48.856978, 2.342782),
+            // mapTypeId: google.maps.MapTypeId.ROADMAP
         });
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    let content = locations[i][0] + '<br><br><a href="' + locations[i][3] + '">' + locations[i][4] + '</a>';
+                    console.log(content);
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+
+            // google.maps.event.addListener(marker, 'click', function() {
+            //     window.location.href = this.url;
+            // });
+        }
+
     </script>
 @endsection
