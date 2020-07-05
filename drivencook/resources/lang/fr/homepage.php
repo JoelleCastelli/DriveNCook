efd021addaadbff6c56ec56c362b1132960602a4
Antoine <fevre.pasquet@gmail.com>
2020-07-05 18:26:05 +0200

    update trans

--- drivencook/resources/lang/fr/homepage.php
+++ drivencook/resources/lang/fr/homepage.php
@@ -25,4 +25,16 @@ return [
     'form' => 'Formulaire',
     'contact_info' => 'Contact',
 
+    // modal contact
+    'contact_message' => 'Avez-vous des questions ? N\'hésitez pas à nous contacter directement.',
+    'name' => 'Nom',
+    'your_name' => 'Votre nom',
+    'mail' => 'Email',
+    'your_mail' => 'Votre email',
+    'message' => 'Message',
+    'your_message' => 'Votre message',
+    'send' => 'Envoyer',
+    'send_contact_success' => 'Message envoyé',
+    'send_contact_data_error' => 'Les données envoyées sont incorrectes',
+    'send_contact_server_error' => 'Le message n\'a pas été envoyé, réessayez plus tard ou contactez-nous à cette adresse',
 ];
