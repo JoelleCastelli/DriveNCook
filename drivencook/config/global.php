5edc8ec550b6dfe80c50cf2669eeba3a41e39dd6
Joëlle <castelli.jo@gmail.com>
2020-07-09 18:15:51 +0200

    dish snack translation fix

--- drivencook/config/global.php
+++ drivencook/config/global.php
@@ -4,8 +4,8 @@ return [
     $GLOBALS['DISH_TYPE'] = [
         'hot_dish' => 'warehouse_dishes.hot_dish',
         'cold_dish' => 'warehouse_dishes.cold_dish',
-        'salty_snack' => 'warehouse_dishes.sweet_snack',
-        'sweet_snack' => 'warehouse_dishes.salty_snack',
+        'salty_snack' => 'warehouse_dishes.salty_snack',
+        'sweet_snack' => 'warehouse_dishes.sweet_snack',
         'drink' => 'warehouse_dishes.drink',
     ],
     $GLOBALS['PURCHASE_ORDER_STATUS'] = [
